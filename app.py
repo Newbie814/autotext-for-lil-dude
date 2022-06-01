3d189353a1d354eaab38516aadec95c6a26b9b95
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-06-01 17:07:54 -0400

    initial commit, autotext for liams teeth

---
+++ app.py
@@ -0,0 +1,30 @@
+# from credentials import liamNum
+import requests
+import schedule
+import time
+
+
+
+
+
+
+
+
+
+def send_message():
+  resp = requests.post('https://textbelt.com/text', {
+    'phone': '15128930922',
+    'message': 'This reminder was sent from a Python script. Clean your teeth, Lil Dude! Text me if you receive this message.',
+    'key': 'textbelt'
+  })
+
+  print(resp.json())
+
+
+schedule.every().day.at("17:00").do(send_message)
+# schedule.every(10).seconds.do(send_message)
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
